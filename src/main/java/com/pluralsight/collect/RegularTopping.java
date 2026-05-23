4f9a3314e78c2cf4e50b1feb3745d38082b96a1a
pmvroque15 <pmvroque@gmail.com>
2026-05-23 12:35:09 -0400

    Completed RegularTopping class that extend the Ingredient class

---
+++ src/main/java/com/pluralsight/collect/RegularTopping.java
@@ -0,0 +1,14 @@
+package com.pluralsight.collect;
+
+public class RegularTopping extends Ingredient {
+    public RegularTopping(String name) {
+       //the boolean isExtra will always be FALSE in regular toppings
+        super(name, false);
+    }
+
+    @Override
+    public double getPrice(SandwichSize size) {
+        //always free
+        return 0;
+    }
+}
