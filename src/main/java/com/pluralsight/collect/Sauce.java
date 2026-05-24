a25932c25de6f474425bc524eda13358b400eee2
pmvroque15 <pmvroque@gmail.com>
2026-05-24 13:25:16 -0400

    Made an Override method toString in Sauce class.

--- src/main/java/com/pluralsight/collect/Sauce.java
+++ src/main/java/com/pluralsight/collect/Sauce.java
@@ -9,7 +9,17 @@ public class Sauce extends Ingredient{
     }
 
     @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
     public double getPrice(SandwichSize size) {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Sauce: %s", getName());
     }
 }
