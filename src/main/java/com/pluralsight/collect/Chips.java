497b8508c1613db34d0325ab3c84fa9a23a37ae1
pmvroque15 <pmvroque@gmail.com>
2026-05-24 13:15:34 -0400

    Edited the Chip class and add chiptype as an attribute to the class.

--- src/main/java/com/pluralsight/collect/Chips.java
+++ src/main/java/com/pluralsight/collect/Chips.java
@@ -1,6 +1,16 @@
 package com.pluralsight.collect;
 
 public class Chips implements Product{
+    //The customer can have freedom to choose what kind of "chip type" according to the instructions, and all chips will be the same price.
+    private String chipType;
+
+    public Chips(String chipType) {
+        this.chipType = chipType;
+    }
+
+    public String getChipType() {
+        return chipType;
+    }
 
     @Override
     public double calculatePrice(){
