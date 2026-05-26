9184d31fd76089d1b3f82137fa264e8ab149b720
pmvroque15 <pmvroque@gmail.com>
2026-05-26 00:14:39 -0400

    Cleaned up the UI class and added comments. Also added more code insed the checkout() following how the instructions wants to be like for this functionality.

--- src/main/java/com/pluralsight/collect/UserInterface.java
+++ src/main/java/com/pluralsight/collect/UserInterface.java
@@ -5,8 +5,8 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class UserInterface {
+    //todo make defensive coding on each prompts
     private final Scanner scanner = new Scanner(System.in);
-
 
     public void display() {
         homeScreen();
@@ -47,35 +47,39 @@ public class UserInterface {
             int input = Integer.parseInt(scanner.nextLine());
 
             switch (input) {
+               //Each case makes an instance of an object and adds to the products arrayList, if it was not null.
                 case 1:
                     Product sandwich = createASandwichOrder();
 
                     if (sandwich != null) {
                         order.addProduct(sandwich);
-                        System.out.println("TEST: Sandwich!");
+                        System.out.println("Sandwich is successfully added to your cart.");
                     }
                     break;
                 case 2:
                     Drink drink = createADrinkOrder();
+
                     if (drink != null) {
                         order.addProduct(drink);
+                        System.out.println("Drink is successfully added to your cart.");
                     }
-
                     break;
                 case 3:
                     Chips chips = createAChipsOrder();
 
                     if (chips != null) {
                         order.addProduct(chips);
+                        System.out.println("Chips is successfully added to your cart.");
                     }
-                    System.out.println("TEST: chips!");
                     break;
                 case 4:
                     checkout(order);
+
                     break;
                 case 0:
                     cancelOrder(order);
                     running = false;
+
                     break;
                 default:
                     System.err.printf("Invalid input: %s. Try again.", input);
@@ -84,35 +88,43 @@ public class UserInterface {
     }
 
     public Product createASandwichOrder() {
-//        //todo: Ask David if it's better to make a method for this. CreateASandwich class?
+     //todo: Ask David if it's better to make a method for this. CreateASandwich class?
+
+        //For making the Sandwich base
         SandwichSize size = chooseSandwichSize();
         BreadType breadType = chooseBreadType();
         boolean isToasted = isToasted();
         Sandwich sandwich = new Sandwich(size, breadType, isToasted);
 
+        //Making an instance of meat through the method chooseAMeat(size) and adding it to the ArrayList of products
         Meat meat = chooseAMeat(size);
         sandwich.addIngredient(meat);
 
+        //Same with Cheese
         Cheese cheese = chooseACheese(size);
         sandwich.addIngredient(cheese);
 
+        //Making a Hashset for toppings because it helps for duplication and separates from the list pf products since user can order multiple toppings.
         HashSet<RegularTopping> regularToppings = chooseRegularToppings();
         for (RegularTopping i : regularToppings) {
             sandwich.addIngredient(i);
         }
 
+        //Sauce
         Sauce sauce = chooseASauce();
         sandwich.addIngredient(sauce);
-
+        //Side
         Side side = chooseASide();
         sandwich.addIngredient(side);
 
-//        //todo make a defensive code make sure arguments are NOT null
         return sandwich;
 
     }
 
+    //for sauce and sides, made an array to iterate through the method chooseFromMenu()
+    //that way, user can see what is the available choices of each category
     public Side chooseASide() {
+
         String[] sides = {"Au jus", "Sauce"};
 
         String side = chooseFromMenu("Choose a side: ", sides);
@@ -121,6 +133,7 @@ public class UserInterface {
     }
 
     public Sauce chooseASauce() {
+
         String[] sauces = {"Mayo", "Mustard", "Ketchup", "Ranch", "Thousand Islands", "Vinaigrette"};
 
         String sauce = chooseFromMenu("Choose your sauce: ", sauces);
@@ -128,19 +141,25 @@ public class UserInterface {
         return new Sauce(sauce);
     }
 
+    //returning hashset of toppings to reuse the data to the createASandwich()
     public HashSet<RegularTopping> chooseRegularToppings() {
+
         HashSet<RegularTopping> toppingsList = new HashSet<>();
+
         String[] toppings = {"Lettuce", "Peppers", "Onions",
                 "Tomatoes", "Jalapeños", "Cucumbers",
                 "Pickles", "Guacamole", "Mushrooms"};
 
         boolean running = true;
+
         while (running) {
+
             String topping = chooseFromMenu("Pick a topping: ", toppings);
 
             if (topping.isBlank()) {
                 running = false;
             }
+
             boolean duplicate = false;
 
             for (RegularTopping t : toppingsList) {
@@ -152,12 +171,12 @@ public class UserInterface {
                 }
             }
 
-
+            //if not a duplicated, add it to a new instance of RegularTopping
             if (!duplicate) {
                 toppingsList.add(new RegularTopping(topping));
             }
 
-            System.out.println("Do you want to add another topping? (Y-yes or N-no)");
+            System.out.println("Do you want to add another topping? (y/n)");
 
             String input = scanner.nextLine();
 
@@ -170,17 +189,27 @@ public class UserInterface {
     }
 
     public Cheese chooseACheese(SandwichSize size) {
+
         String[] kindOfCheese = {"American", "Provolone", "Cheddar", "Swiss"};
+
         String cheese = chooseFromMenu("Pick your cheese", kindOfCheese);
 
-        System.out.printf("Do you want extra of %s? (Y-yes or N-no)", cheese);
-        boolean isExtra = scanner.nextLine().equalsIgnoreCase("y");
+        //if input is not null, return the instance of Cheese, otherwise, return null
+        if (cheese != null) {
 
-        return new Cheese(cheese, size, isExtra);
+            System.out.printf("Do you want extra of %s? y/n", cheese);
+
+            boolean isExtra = scanner.nextLine().equalsIgnoreCase("y");
+
+            return new Cheese(cheese, size, isExtra);
+        }
+
+        return null;
+
     }
 
     public Boolean isToasted() {
-        System.out.println("Would you like the sandwich toasted? (Yes or No)");
+        System.out.println("Would you like the sandwich toasted? (y/n)");
         String toasted = scanner.nextLine();
 
         return toasted.equalsIgnoreCase("Yes");
@@ -190,9 +219,12 @@ public class UserInterface {
         boolean running = true;
 
         SandwichSize sandwichSize = null;
+
         do {
+
             System.out.println("Sandwich size: (4, 8, 12)");
             int size = Integer.parseInt(scanner.nextLine());
+
             switch (size) {
                 case 4:
                     sandwichSize = SandwichSize.FOUR_INCH;
@@ -257,13 +289,16 @@ public class UserInterface {
 
         String meat = chooseFromMenu("Choose your protein", meats);
 
-        System.out.printf("Do you want extra of %s? y/n", meat);
-        boolean isExtra = scanner.nextLine().equalsIgnoreCase("y");
+        if (meat != null) {
+            System.out.printf("Do you want extra of %s? y/n", meat);
+            boolean isExtra = scanner.nextLine().equalsIgnoreCase("y");
+            return new Meat(meat, size, isExtra);
+        }
 
-        return new Meat(meat, size, isExtra);
+        return null;
     }
 
-    //helper method
+    //HELPER METHOD: Going back to Sides and Sauces, made a method instead of repeating the logic.
     public String chooseFromMenu(String prompt, String[] options) {
 
         while (true) {
@@ -278,7 +313,7 @@ public class UserInterface {
             String input = scanner.nextLine();
 
             if (input.isEmpty()) {
-                return "";
+                return null;
             }
 
             for (String option : options) {
@@ -292,25 +327,18 @@ public class UserInterface {
     }
 
     public Chips createAChipsOrder() {
-//        System.out.println("What you like to add a bag of chips? (Y-yes, N-no or Enter to skip)");
-//        String input = scanner.nextLine();
-
-//        if (input.isBlank()) {
-//            return null;
-//        }
-
+        //Just returned the instance of Chips since it does not need anything else and all chips are $1.50
         return new Chips();
     }
 
     public Drink createADrinkOrder() {
-        //What if the user wants more than one drink?
 
         DrinkSize size = null;
 
         while (size == null) {
             try {
-                    System.out.println("What size? (Small, Medium, Large)");
-                    size = DrinkSize.valueOf(scanner.nextLine().toUpperCase());
+                System.out.println("What size? (Small, Medium, Large)");
+                size = DrinkSize.valueOf(scanner.nextLine().toUpperCase());
 
             } catch (IllegalArgumentException e) {
                 System.err.println("Invalid size. Try again.");
@@ -327,14 +355,25 @@ public class UserInterface {
     public void cancelOrder(Order order) {
         order.cancelOrder();
 
-        System.out.println("Order Canceled");
+        System.out.println("Order Canceled.");
     }
 
     public void checkout(Order order) {
-//        //todo Make sure the math is right AND it populates accurately.
-//        //todo render the order then confirms or cancel the order before completing the order
-//        order.completeOrder();
-        System.out.println(order.toString());
+        System.out.println(order);
+        System.out.println("Place your order? Type \"y\" to check out, \"n\" to go back to the menu, \"x\" to cancel the order.");
+
+        String input = scanner.nextLine();
+
+        if(input.equalsIgnoreCase("y")) {
+            order.completeOrder();
+            System.out.println("Order places successfully! Thank you!");
+        } else if(input.equalsIgnoreCase("x")) {
+            cancelOrder(order);
+            System.out.println("Order is canceled.");
+        }
+        else {
+            System.out.println("Returning to the menu...");
+        }
     }
 
 
